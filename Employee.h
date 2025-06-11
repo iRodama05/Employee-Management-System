5e69095db15bb2347ff97e7426b3d6c00f4d7ba2
iRodama05 <iker.rdz.amaro@gmail.com>
2025-06-10 23:08:47 -0600

    Update Employee.h

--- Employee.h
+++ Employee.h
@@ -2,23 +2,39 @@
 #define EMPLOYEE_H
 
 #include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
 
 class Employee {
 protected:
-    std::string name;
+    string name;
     int id;
     double baseSalary;
 
 public:
-    Employee(std::string name, int id, double baseSalary);
-    virtual ~Employee(); // destructor virtual
-    std::string getName() const;
+    // Constructor
+    Employee(string name, int id, double baseSalary);
+    virtual ~Employee(); // Destructor
+
+    // Getters
+    string getName() const;
     int getId() const;
     double getBaseSalary() const;
+
+    // Setters
+    void setName(string n);
+    void setId(int i);
     void setBaseSalary(double salary);
-    
-    virtual double calculateTotalSalary() const = 0; // método abstracto
-    virtual void printInfo() const = 0;              // método virtual
+
+    // Virtual methodes
+    virtual double calculateBonus() const;
+    virtual double calculateTotalSalary() const = 0;
+    virtual string getInfo() const = 0;
+
+    // Operator overloading
+    bool operator==(Employee& other) const;
+    friend ostream& operator<<(ostream& os, const Employee& e);
 };
 
 #endif
